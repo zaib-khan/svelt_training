929d3f7a6ec7e7ac2b26cfebb423cac664df5ae8
zaib khan <zaib16khan@gmail.com>
2022-06-03 16:40:46 +0200

    debut de l'exo

--- exercice/exo_1/src/App.svelte
+++ exercice/exo_1/src/App.svelte
@@ -1,5 +1,28 @@
 <script>
   import "./assets/css/style.css";
+  const tabLegumes = [
+    { name: "Aubergine", season: "automne" },
+    { name: "Brocoli", season: "automne" },
+    { name: "Oignon", season: "automne" },
+    { name: "Tomate", season: "automne" },
+    { name: "Radis", season: "printemps " },
+    { name: "Céleri", season: "printemps" },
+    { name: "Carotte", season: "printemps" },
+    { name: "Potiron", season: "Hiver" },
+    { name: "Poireau", season: "Hiver" },
+    { name: "Châtaigne", season: "Hiver" },
+  ];
+
+  let isListDisplayed = true;
+
+  const btnClickEventShowList = () => {
+    if (!isListDisplayed) isListDisplayed = true;
+    console.log(isListDisplayed);
+  };
+  const btnClickEventShowOneVegetable = () => {
+    if (isListDisplayed) isListDisplayed = false;
+    console.log(isListDisplayed);
+  };
 </script>
 
 <div class="bg-slate-50 flex flex-col min-h-screen">
@@ -10,7 +33,29 @@
   </header>
   <main class="container mx-auto m-8 border-2 border-dashed rounded p-8">
     <!-- CONTENU ICI -->
-    Votre contenu ici !
+    <div class=" flex justify-between">
+      <button
+        class=" bg-orange-500 rounded p-2 text-white hover:bg-orange-800 duration-200"
+        on:click={btnClickEventShowList}>Show the liste of vegetable</button
+      >
+      <button
+        class=" bg-blue-500 rounded p-2 text-white hover:bg-blue-800 duration-200"
+        on:click={btnClickEventShowOneVegetable}>Show me one vegetable</button
+      >
+    </div>
+    {#if isListDisplayed}
+        {#each tabLegumes as { isConnect, time }, index}
+          <p class=" mt-2 ml-4">
+            {index + 1}. {isConnect ? "Connexion" : "Déconnexion"} à {time}
+          </p>
+        {/each}
+      {/if}
+    {:else}
+      <button
+        class=" px-4 py-2 rounded shadow bg-green-600 text-white duration-200 hover:bg-green-900"
+        on:click={toggleLogin}>Log in</button
+      >
+    {/if}
   </main>
 </div>
 
